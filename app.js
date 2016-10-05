c9d772e1819118b90e4fa24a06c0add25d36fd4f
Kaustabh04 <kaustabh04@gmail.com>
2016-10-06 00:16:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+  // Create the variable
+  
+  var radius = 10; 
+  var area = radius * radius * Math.PI;
+
+  // Print the area of the circle in console
+  
+  console.log("Area of the circle is " + area)
+  
